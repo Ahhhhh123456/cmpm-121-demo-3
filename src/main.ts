4b7524d5603421864fc3cd8c5c4293fd0315f6c0
Jason Li <Jason.Li8885551@gmail.com>
2024-11-04 20:10:56 -0800

    Button Made.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-// todo
+const gameName = "Jason's Game :)";
+
+//New button element
+const button = document.createElement("button");
+
+//Button text
+button.textContent = "Test";
+
+// BUtton styles
+button.style.backgroundColor = "black";
+button.style.color = "white";
+button.style.padding = "10px 20px";
+
+// Even listener for the button
+button.addEventListener("click", function () {
+  alert("Button clicked.");
+});
+
+// Adds the button to the website
+document.body.appendChild(button);
+document.title = gameName;
